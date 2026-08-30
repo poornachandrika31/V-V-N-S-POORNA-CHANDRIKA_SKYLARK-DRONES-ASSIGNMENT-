3f675975afd01886a286ba38da4957cc521d0c41
poornachandrika31 <chandrikavvns31@gmail.com>
2026-08-30 14:43:39 +0530

    Fix CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,8 +22,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://v-v-n-s-poorna-chandrika-skylark-drones-assignment-5qnzkmops.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
